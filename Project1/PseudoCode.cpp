a2dc1d67d09005ffec02ca909e8e14ce6c881f14
clay bro <claybro2005@gmail.com>
2021-03-07 14:56:44 -0500

    wrote a little more pseudo code

--- Project1/PseudoCode.cpp
+++ Project1/PseudoCode.cpp
@@ -11,25 +11,60 @@ struct com{
 	string preamble = "main:\n\tpushq\t%rbp\n\tmoveq\t%rsp, %rbp\n";
 } line;
 
+int funct_state(string line){
+	write preamle
+	parse the line for:
+	{
+	   return_type;
+	   find first space;
+	   function name;
+	   "\("
+           call var_dec(rest of line);
+	}
+	return
+}
+int var_dec(string line){
+	parse for spaces or "\)": 
+
+	if int -> size=8;
+	
+
+
 int main( int argc,  char **argv){  
  
     if(argc != 3 ){ //./compiler input_code.c output_code.s 
    	//check for input file 
    	return error message 
     } 
-   
  
     ifstream in_code(argv[1] ); //open the file to read from 
     ofstream out_code(argv[2] ); //open the assembly code file to write into 
    
-
     string line; 
+    
+    for line in input_file{
+	    if starts with (int)
+	    {
+		could be function declaration -> call funct_state(line)
+	        could be variable declaration -> call var_dec(line);
+	    } 
+	    if starts with "if"
+	    {
+                call if_state(line);
+	    }
+	    if starts with "for"
+	    {
+		call for_state(line);
+	    }
+	    if starts with "var_name":
+	    {
+	    	call arith_state(line);
+	    }
+	}	    
+    
    
  
     //write the preamble 
-   
-
-     while(getline(in_code, line)){ //iterate over every line in input file 
    
 
      // bulk of the work for next time 
@@ -41,7 +76,7 @@ int main( int argc,  char **argv){
      } 
    
 
-     //write the post script 
+     //write the epilogue 
    
 
      in_code.close(); 
